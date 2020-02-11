0e47cf6ce32654d7ee0c23eb318fbde1ed4dcb34
Oliver Ofenloch <oliver.ofenloch@email.de>
2020-02-11 06:41:59 +0000

    add simple parsing of xml file

--- bin/include/main.h
+++ bin/include/main.h
@@ -1,6 +1,8 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
+#include <xmlutils.h>
+
 #include <mainlib.h>
 
 #endif // #ifndef _MAIN_H_
--- bin/source/main.cpp
+++ bin/source/main.cpp
@@ -4,9 +4,20 @@ int main(int argc, char *argv[])
 {
   CHello myGreeter;
 
-  myGreeter.sayHello();
+  myGreeter.sayHello("terev - Test Runner and Evaluator");
 
-  myGreeter.sayHello("Hello, World!");
-  
+  std::string xmlFileName = "./data/test01.xml";
+
+  Poco::AutoPtr<Poco::XML::Document> pXMLDoc = xmlutils::parseFile(xmlFileName);
+  if (pXMLDoc)
+  {
+    std::cout << "Parsed file \"" << xmlFileName << "\"\n";
+    const Poco::XML::DocumentType *pDocType = pXMLDoc->doctype();
+  }
+  else
+  {
+    std::cout << "Parsing file \"" << xmlFileName << "\" failed!\n";
+  }
+
   return 0;
 } // int main(int argc, char *argv[])
